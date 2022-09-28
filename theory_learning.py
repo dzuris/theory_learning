33b75662859713b12b957c2f9b8cc598c6ffa02d
dzuris <adamdzurilla19@gmail.com>
2022-09-28 20:49:21 +0200

    launch.json

--- theory_learning.py
+++ theory_learning.py
@@ -18,12 +18,20 @@ ERROR_WRONG_PROGRAM_ARGUMENTS = 11
 ERROR_WRONG_XML_STRUCTURE = 20
 
 
-def clear():
+def clear_terminal():
+    """
+    The function clears the terminal
+    :return:
+    """
     os.system('cls' if os.name == 'nt' else 'clear')
     return "   "
 
 
 def print_help_message():
+    """
+    The function prints the help message
+    :return:
+    """
     print("Usage:")
     print("\tpython3.8 theory_learning.py [FILE | help]")
     print()
@@ -47,6 +55,10 @@ def print_help_message():
 
 
 def print_choices_help():
+    """
+    The function prints the help for choices
+    :return:
+    """
     print("Choices:")
     print("\tr | random - random question")
     print("\t[NUMBER] - gets question from position, indexing starts from one (e.g. 20)")
@@ -60,49 +72,95 @@ def print_choices_help():
 
 
 class Subject:
+    """
+    Class for loading xml and handling with data
+    """
     list_of_quest_and_answer = {}
     value = None
 
     def __init__(self, input_file):
+        """
+        The constructor class
+        :param input_file: Xml file which contains data
+        """
         self.load_from_file(input_file)
 
     def load_from_file(self, input_file):
+        """
+        Loads XML file into list
+        :param input_file: Input file which contains data
+        :return:
+        """
         tree = ET.parse(input_file)
         root = tree.getroot()
 
+        # Checks root attribute
         if root.tag != 'data':
             print('Wrong xml structure')
             exit(ERROR_WRONG_XML_STRUCTURE)
-            
+
+        # Iterates through elements in tree and add them to the list
         for element in root.findall('element'):
             question = element.find('question').text
             answer = element.find('answer').text
             self.list_of_quest_and_answer[question] = answer
 
     def get_question(self):
+        """
+        Gets question by value
+        :return: Value from class
+        """
         return self.value
 
     def get_answer(self):
+        """
+        Gets answer from question
+        :return: Answer from concrete question
+        """
         return self.list_of_quest_and_answer[self.value]
 
     def get_count(self):
+        """
+        Gets number of questions in list
+        :return: Integer count number
+        """
         return len(self.list_of_quest_and_answer)
 
     def get_question_from_position(self, position: int):
+        """
+        Gets question from concrete position, if position is valid
+        :param position: Integer position
+        :return: Question
+        """
         if self.get_count() >= position > 0:
             keys_list = list(self.list_of_quest_and_answer)
             key = keys_list[position - 1]
             return key
 
     def select_random(self):
+        """
+        Selects random question from the list, and sets the value by the choice
+        :return:
+        """
         self.value = random.choice(list(self.list_of_quest_and_answer))
 
     def print_ran_choice(self):
+        """
+        Selects random choice and then prints it
+        :return:
+        """
         self.select_random()
         print(self.get_question())
 
     def print_from_position(self, position):
+        """
+        Prints the question from selected position
+        :param position: Position
+        :return:
+        """
         key = self.get_question_from_position(position)
+
+        # Checks position validity
         if key is None:
             print('Wrong index (indexing starts from 1 to ' + str(self.get_count()) + ')')
         else:
@@ -110,15 +168,29 @@ class Subject:
             print(key)
 
     def print_answer(self):
+        """
+        Prints the answer of question in value
+        :return:
+        """
+
+        # Checks if value is not None
         if self.value is not None:
             print(self.get_answer())
         else:
             print('No question was generated yet, see \'help\' for how to generate a question')
 
     def print_count(self):
+        """
+        Prints the number of questions
+        :return:
+        """
         print(self.get_count())
 
     def print_all(self):
+        """
+        Prints the all questions from the list
+        :return:
+        """
         num = 1
         for key in self.list_of_quest_and_answer:
             print(str(num) + ":\t" + key)
@@ -145,7 +217,8 @@ if __name__ == "__main__":
 
     subject = Subject(file)
 
-    clear()
+    clear_terminal()
+    print_choices_help()
     run = True
     while run:
         choice = None
@@ -156,18 +229,18 @@ if __name__ == "__main__":
             continue
 
         if choice == 'r' or choice == 'random':
-            clear()
+            clear_terminal()
             subject.print_ran_choice()
         elif choice == 'a' or choice == 'answer':
             subject.print_answer()
         elif re.match("[0-9]+", choice):
-            clear()
+            clear_terminal()
             subject.print_from_position(int(choice))
         elif choice == 'c' or choice == 'count':
-            clear()
+            clear_terminal()
             subject.print_count()
         elif choice == 'clear':
-            clear()
+            clear_terminal()
         elif choice == 'all':
             subject.print_all()
         elif choice == 'quit':
